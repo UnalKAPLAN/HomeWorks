6c215aaced665c23c8cc484335c296dd13bb8bb6
UnalKAPLAN <82314934+UnalKAPLAN@users.noreply.github.com>
2021-05-01 21:13:41 +0300

    reCapDemo_Classes

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,14 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FourOperations fourOperations=new FourOperations();
+		int result=fourOperations.Add(3,4);
+		System.out.println(result);
+
+	}
+
+}
